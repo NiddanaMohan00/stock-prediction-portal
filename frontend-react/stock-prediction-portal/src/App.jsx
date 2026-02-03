d8769fe21df0be6c924d17fc8d478281dfc9da8b
niddana mohan <you@example.com>
2026-02-03 23:48:24 +0530

    manage react

--- frontend-react/stock-prediction-portal/src/App.jsx
+++ frontend-react/stock-prediction-portal/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
-
-
+import './assets/css/style.css'
+import Header from './componen/Header'
+import Main from './componen/Main'
+import Footer from './componen/Footer'
 
 function App() {
   
 
   return (
     <>
+    <Header/>
+    <Main/>
+    <Footer/>
     </>
   )
 }
