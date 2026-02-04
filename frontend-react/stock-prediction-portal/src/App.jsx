e600d176b6d23022fe45d9fb21e7daf29cd2768b
niddana mohan <you@example.com>
2026-02-05 01:06:04 +0530

    registration page

--- frontend-react/stock-prediction-portal/src/App.jsx
+++ frontend-react/stock-prediction-portal/src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
 import './assets/css/style.css'
-import Header from './componen/Header'
 import Main from './componen/Main'
+
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Register from './componen/Register'
+
+import Header from './componen/Header'
 import Footer from './componen/Footer'
+import Login from './componen/Login'
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
     <Header/>
-    <Main/>
+    <Routes>
+      <Route path='/' element={<Main/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
     <Footer/>
+    </BrowserRouter>
+   
+   
+ 
     </>
   )
 }
--- frontend-react/stock-prediction-portal/src/componen/Main.jsx
+++ frontend-react/stock-prediction-portal/src/componen/Main.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Button from './Button'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 const Main = () => {
   return (
     <>
